77272c77971d8cb48e7b302fa2dd5f568e34fb73
Hammod-M <hammod_13@mail.ru>
2023-10-26 19:16:39 +0500

    fixed reload button

--- src/components/quiz/Result.js
+++ src/components/quiz/Result.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from "react";
 
-export const Result = ({questions, correctSum}) => {
+export const Result = ({ questions, correctSum }) => {
   return (
     <div className="result">
-      <img src={correctSum === questions.length ? 'https://cdn-icons-png.flaticon.com/512/2278/2278992.png' : 'https://free-png.ru/wp-content/uploads/2021/05/free-png.ru-21-340x340.png' } />
-      <h2>Вы отгадали {correctSum} ответа из {questions.length}</h2>
-      <a href="/">
-        <button>Попробовать снова</button>
-      </a>
+      <img
+        src={
+          correctSum === questions.length
+            ? "https://cdn-icons-png.flaticon.com/512/2278/2278992.png"
+            : "https://free-png.ru/wp-content/uploads/2021/05/free-png.ru-21-340x340.png"
+        }
+        alt="img"
+      />
+      <h2>
+        Вы отгадали {correctSum} ответа из {questions.length}
+      </h2>
+
+      <button onClick={() => window.location.reload()}>
+        Попробовать снова
+      </button>
     </div>
-  )
-}
+  );
+};
